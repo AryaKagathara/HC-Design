3cb19ceb3b5db0d91501dbe48d56d572236199d2
AryaKagathara <iamaryak27@gmail.com>
2021-01-29 14:56:34 +0530

    Start

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+var option = {
+    stringsElement: '.texts',
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true,
+    loop: true,
+    autoInsertCss: true,
+    cursorChar: ""
+};
+
+var typed = new Typed('#elements', option);   
